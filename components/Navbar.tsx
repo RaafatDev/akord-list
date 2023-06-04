585b1cd111defee0214e9173436add62fa6ee966
Raafat-itu <raafat@innovation-through-understanding.de>
2023-06-04 18:49:23 +0200

    added '@tailwindcss/typography' package && general adjustments in the main-view

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+    return (
+        <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+            <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+                <Link href="/" className="text-white/90 no-underline hover:text-white">
+                    Home
+                </Link>
+            </div>
+        </nav>
+    );
+};
